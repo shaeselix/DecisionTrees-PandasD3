c5c1737f53ad1d3b5358cc5617599dd8cc6ad306
shaeselix <shae.selix@gmail.com>
2016-02-28 22:06:03 -0800

    weekend edits

--- DecisionTreeC45.py
+++ DecisionTreeC45.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import collections
+import webbrowser
+import os
 
 def I(counts):
         i = 0
@@ -43,42 +45,51 @@ class DecisionTree(object):
         self.nid = 0
         self.nodes = {}
         self.queue = collections.deque()
-        self.queue.append(Node([], self.x, self.ycc))
+        self.queue.append(Node([], self.x, self.ycc, 'root'))
         
         while self.queue:
             
             node_i = self.queue.popleft()
             
+            if len(node_i.Xnames) == 0 or any([v == 0 for v in node_i.yc_i.values()]):
+                print('breaking')
+                print(node_i.pathkeys, node_i.yc_i)
+                node_i.isleaf = True
+                node_i.decision = max(node_i.yc_i.items(), key = lambda x: x[1])[0]
+                self.nodes[self.nid] = node_i
+                self.nid += 1
+                continue
+            
             Info_y = I(node_i.yc_i.values())
+            
+            #print(node_i.Xnames)
             
             ct = self.ContinGENcyTable(node_i.pathkeys, node_i.Xnames)
             maxgain = 0
             
             T = sum(node_i.yc_i.values())
             for c in ct:
+                print(c)
                 Info_yx = np.sum(np.multiply(
                         np.apply_along_axis(I, 1, c[1]),
                         np.apply_along_axis(np.sum, 1, c[1]) / T
                         ))
                 Gain_x = Info_y - Info_yx
-                if Gain_x > maxgain:
+                if Gain_x >= maxgain:
                     self.chosen = c
                     maxgain = Gain_x
-            print((self.chosen, maxgain))
-            node_i.opt = c[0]
-            node_i.levels = c[2]
+            #print((self.chosen, maxgain))
+            node_i.opt = self.chosen[0]
+            print(node_i.opt)
+            node_i.levels = self.chosen[2]
+            Xnew = [x for x in node_i.Xnames if x != self.chosen[0]]
+            print(Xnew)
             for i in range(len(node_i.levels)):
-                path = node_i.pathkeys[:] + [(c[0],'=',c[2][i])]
+                path = list(node_i.pathkeys) + [(self.chosen[0],'=',self.chosen[2][i])]
                 ycci = {}
                 for j, key in enumerate(self.ygroup):
-                    ycci[key] = c[1][i][j]
-                if all([v > 0 for v in ycci.values()]) and len(node_i.Xnames) > 1:
-                    print('continuing')
-                    Xnew = [x for x in node_i.Xnames if x != c[0]]
-                    self.queue.append(Node(path, Xnew, ycci))
-                else:
-                    print('stopping')
-                    print(path, ycci)
+                    ycci[key] = self.chosen[1][i][j]
+                self.queue.append(Node(path, Xnew, ycci, self.nid))
             self.nodes[self.nid] = node_i
             self.nid += 1
                 
@@ -86,7 +97,6 @@ class DecisionTree(object):
     
     def ContinGENcyTable(self, pathkeys, X):
         df_i = self.df
-        print(pathkeys)
         for p in pathkeys:
             df_i = df_i.loc[df_i[p[0]] == p[2]]
         for x in X:
@@ -97,6 +107,35 @@ class DecisionTree(object):
                 for j, yc in enumerate(self.ygroup):
                     A[i][j] = np.sum(np.in1d(xgroup[xc],self.ygroup[yc],assume_unique=True))
             yield (x, A, levels)
+            
+    def D3tree(self, filename, openpage=True):
+        hier = {}
+        for key in self.nodes:
+            if self.nodes[key].isleaf:
+                parent = self.nodes[key].decision
+                hier[key] = {'name': parent, 'children': None}
+            else:
+                parent = self.nodes[key].opt
+                d = [nid for nid, n in self.nodes.items() if n.parent == key]
+                hier[key] = {'name': parent, 'children': d}
+        higherkeys = list(hier.keys())
+        higherkeys.reverse()
+        for key in higherkeys:
+            newlist = []
+            if hier[key]['children']:
+                for bbk in hier[key]['children']:
+                    newlist.append(hier[bbk])
+                    hier[key]['children'] = newlist
+            else:
+                del hier[key]['children']
+        htmlf = 'TreeDiagrambase.html'
+        red = open(htmlf, 'r').read()
+        s = red % str(hier[0])
+        htmlf = open(filename, 'w')
+        htmlf.write(s)
+        htmlf.close()
+        if openpage:
+            webbrowser.open_new_tab('file://'+os.getcwd()+'/'+filename)
     
 
             
@@ -105,12 +144,14 @@ class DecisionTree(object):
 #Decision tree data is held by intelligent nodes (that can also be leaves)                
 class Node(object):
     
-    def __init__(self, pathkeys, Xnames, yc_i, isleaf = False, ):
+    def __init__(self, pathkeys, Xnames, yc_i, parent, isleaf = False):
         self.isleaf = isleaf
+        self.decision = None
         self.pathkeys = pathkeys
         self.Xnames = Xnames
         self.yc_i = yc_i
         self.opt = None
         self.levels = None
+        self.parent = parent
     
         
